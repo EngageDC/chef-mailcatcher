2206d521294659d93a33e17feeebf26869daad11
Mathias Hansen <me@codemonkey.io>
2015-01-30 17:04:36 -0500

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,7 +14,7 @@ case node['platform_family']
         package "g++"
         package "ruby-dev"
     when "rhel", "fedora", "suse"
-        package "sqlite"
+        package "sqlite sqlite-devel"
 end
 
 # Install MailCatcher
